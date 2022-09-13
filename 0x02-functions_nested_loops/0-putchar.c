def81b5be8f4dfb8db58695560e744ce85187fe9
hesbornO <46963355+hesbornO@users.noreply.github.com>
2022-09-13 19:44:07 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
-
+#include "main.h"
 /**
- * main - Prints _putchar
- *
- * Return: Always 0 (Success)
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
 	return (0);
 }
